e46f57ae6e0cec9b39ac490078ab9bffa5aaf310
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-11-11 16:12:42 +0900

    add export functions

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { fixFromFile, fixWithPattern,
          lint, lintFromFile, lintWithPattern } from './lint';
 import { LintOut, code2String, makeSeverity, makeFullSeverity } from './output';
 import { Pattern } from './patterns';
+import { makePatterns, makePatternsFromChunk, makePatternsFromDiff, Identifier } from './diff/makePatterns';
+import { makeDiffObj } from './diff/diffparser';
+
 export { code2String, extend, fixFromFile, fixWithPattern,
-         LintOut, lint, lintFromFile, lintWithPattern, makeSeverity, makeFullSeverity, Pattern };
+         LintOut, lint, lintFromFile, lintWithPattern, makeSeverity, makeFullSeverity, Pattern, makePatterns, makePatternsFromChunk, makePatternsFromDiff, makeDiffObj, Identifier };
