178795a9d6233f7759f27d828debc59c512874cc
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-11-15 21:40:14 +0900

    remove makediffobj

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import { fixFromFile, fixWithPattern,
          lint, lintFromFile, lintWithPattern } from './lint';
 import { LintOut, code2String, makeSeverity, makeFullSeverity } from './output';
 import { Pattern } from './patterns';
-import { makeDiffObj } from './make-patterns/diffparser';
+// import { makeDiffObj } from './make-patterns/diffparser';
 
 // import { makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs, Identifier } from './make-patterns/makePatterns';
 // import { mineRules, mineRulesDetail } from './addRules';
 
 export { code2String, extend, fixFromFile, fixWithPattern,
          LintOut, lint, lintFromFile, lintWithPattern, makeSeverity, makeFullSeverity,
-         Pattern,  makeDiffObj,
+         Pattern,  
+        //  makeDiffObj,
         //  makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs,
         //  Identifier, mineRules, mineRulesDetail 
         };
