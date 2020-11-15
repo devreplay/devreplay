bb2e07aebc94efc7755dd04bae86c1a3ddff2393
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-11-15 21:36:10 +0900

    remove unused exports

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { fixFromFile, fixWithPattern,
          lint, lintFromFile, lintWithPattern } from './lint';
 import { LintOut, code2String, makeSeverity, makeFullSeverity } from './output';
 import { Pattern } from './patterns';
-import { makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs, Identifier } from './make-patterns/makePatterns';
 import { makeDiffObj } from './make-patterns/diffparser';
-import { mineRules, mineRulesDetail } from './addRules';
+
+// import { makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs, Identifier } from './make-patterns/makePatterns';
+// import { mineRules, mineRulesDetail } from './addRules';
 
 export { code2String, extend, fixFromFile, fixWithPattern,
          LintOut, lint, lintFromFile, lintWithPattern, makeSeverity, makeFullSeverity,
-         Pattern, makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs, makeDiffObj,
-         Identifier, mineRules, mineRulesDetail };
+         Pattern,  makeDiffObj,
+        //  makePatterns, makePatternsFromChunk, makePatternsFromDiff, makePatternsFromDetailedDiffs,
+        //  Identifier, mineRules, mineRulesDetail 
+        };
