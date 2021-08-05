cedc7994cd8fe47a98063d2baa9435a52b50a7ce
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-08-05 16:06:09 +0900

    remove unused info from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,3 @@ export { extend } from './lib/extend';
 export { fix, fixWithRules, lint, lintWithRules } from './lib/lint';
 export { LintOut, code2String, makeSeverity, makeFullSeverity } from './lib/output';
 export { Rule } from './lib/rule';
-export { makeDiffObj } from './lib/rule-maker/diffparser';
-
-export { makeRules, makeRulesFromChunk, makeRulesFromDiff, makeRulesFromDetailedDiffs, Identifier } from './lib/rule-maker/makeRules';
-export { mineProjectRules, mineProjectRulesDetail } from './lib/rule-maker/mineProjectRules';
