333d3d85d3c30292239d6d0ed55e914f36c3d48b
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-08-31 15:22:29 +0900

    add functions for index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { extend } from './lib/extend';
 export { fix, fixWithRules, lint, lintWithRules } from './lib/lint';
 export { LintOut, code2String, makeSeverity, makeFullSeverity } from './lib/output';
 export { BaseRule as Rule, DevReplayRule, RuleSeverity } from './lib/rule';
-export { readCurrentRules, readRuleFile, writeRuleFile } from './lib/ruleManager';
+export { readCurrentRules, readRuleFile, writeRuleFile, BaseRule2DevReplayRule, readExtends } from './lib/ruleManager';
